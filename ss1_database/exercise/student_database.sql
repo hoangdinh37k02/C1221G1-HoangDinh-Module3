32b5403d735bcb981a6d397dc5140296a998ac67
unknown <hoangdinh37k02@gmail.com>
2022-03-28 16:26:48 +0700

    exercise

--- ss1_database/exercise/student_database.sql
+++ ss1_database/exercise/student_database.sql
@@ -1,20 +1,19 @@
 -- drop DATABASE `student-management`;
 CREATE DATABASE `student-management`;
-
 use `student-management`;
 create table `student`(
-`id` int primary key,
-`name` varchar(50) null,
-`age` int null,
-`country` varchar(45) null
+`id_student` int primary key,
+`name` varchar(50),
+`age` int,
+`country` varchar(45)
 );
 create table `class`(
-`id` int primary key,
-`name` varchar(50) null
+`id_class` int primary key,
+`name` varchar(50)
 );
 create table `teacher`(
 `id` int primary key,
-`name` varchar(50) null,
-`age` int null,
-`country` varchar(45) null
+`name` varchar(50),
+`age` int,
+`country` varchar(45)
 );
